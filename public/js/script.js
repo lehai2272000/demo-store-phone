c29b03c497960d624260d89e9ae0e463bac6db16
unknown <you@example.com>
2023-04-10 11:52:48 +0700

    commit navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
+const addressForm = document.querySelector('#address-form');
+const formClose = document.querySelector('.form-close');
+
+addressForm.addEventListener("click", function(){
+    document.querySelector('.address-form').style.display = "flex";
+})
+
+formClose.addEventListener("click", function(){
+    document.querySelector('.address-form').style.display = "none";
+})
+
 const rightBtn = document.querySelector('.fa-arrow-circle-right');
 const leftBtn = document.querySelector('.fa-arrow-circle-left');
 const imgNumber = document.querySelectorAll('.slide-content-left img')
